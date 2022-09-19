45359b0e5a5bed569b298502c8abfbb50f4d2660
cn3085 <cn3085@gmail.com>
2022-09-19 11:21:09 +0900

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,1 @@
-version: 0.0
-os: linux
-files:
-  - source: /
-    destination: /home/ubuntu/cicd/test
-    overwrite: yes
-permisstions:
-  - object: /
-    pattern: "**"
-    owner: ubuntu
-    group: ubuntu
-hooks:
-  AfterInstall: 
-    - location: scripts/deploy.sh
-      timeout: 60
-#      runas: ubuntu
+echo "success!!!!" > success.txt
