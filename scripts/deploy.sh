bc02feb9f2e77e32c0bff322ae2cd7133f8e92a6
cn3085 <cn3085@gmail.com>
2022-09-21 17:18:36 +0900

    change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
-echo "-----start-----" >> /home/ubuntu/cicd/deploy.log
-ls >> /home/ubuntu/cicd/deploy.log
+echo "-----start-----" > /home/ubuntu/cicd/deploy.log
+ls ./ >> /home/ubuntu/cicd/deploy.log
 sudo docker build ./ -t cicd_test
-ls >> /home/ubuntu/cicd/deploy.log
+echo "-----after build" >> /home/ubuntu/cicd/deploy.log
+ls ./ >> /home/ubuntu/cicd/deploy.log
 sudo docker ps -q --filter "name=cicd_test" | grep -q . && docker container kill cicd_test
 sudo docker run -d --rm --name cicd_test -p 8888:8080 cicd_test
