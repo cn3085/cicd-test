52e3111b8f81a04aec24ff6ebecc2e65f705432e
cn3085 <cn3085@gmail.com>
2022-09-21 10:44:14 +0900

    rm image and change message

--- src/main/java/com/cicd/assignment/controller/HomeController.java
+++ src/main/java/com/cicd/assignment/controller/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
 	@GetMapping("/")
 	public ResponseEntity<?> home(){
 		Map<String, Object> response = new HashMap<>();
-		response.put("data", "change message!");
+		response.put("data", "rm image and change message!");
 		
 		return ResponseEntity.ok(response);
 	}
