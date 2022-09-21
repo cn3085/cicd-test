23394448a5db05ed55bc9b51b2b13d6123e576c8
cn3085 <cn3085@gmail.com>
2022-09-21 15:09:41 +0900

    build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
-docker build ../. -t cicd_test
+docker build ./ -t cicd_test
 docker ps -q --filter "name=cicd_test" | grep -q . && docker container kill cicd_test
 docker run -d --rm --name cicd_test -p 8888:8080 cicd_test
