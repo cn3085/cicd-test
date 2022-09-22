361d64680ded4e5549970f0ba56e11ecb83b597e
cn3085 <cn3085@gmail.com>
2022-09-22 11:24:34 +0900

    add

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,6 @@
+echo ${DEPLOYMENT_GROUP_ID} >> /home/ubuntu/cicd/deploy.log
+echo ${DEPLOYMENT_ID} > /home/ubuntu/cicd/deploy.log
+
 docker build ../. -t cicd_test
 docker ps -q --filter "name=cicd_test" | grep -q . && docker container kill cicd_test
 docker run -d --rm --name cicd_test -p 8888:8080 cicd_test
