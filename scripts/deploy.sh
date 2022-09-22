075451772d79c4079822391a3ad4b51104305b13
cn3085 <cn3085@gmail.com>
2022-09-22 11:02:21 +0900

    push

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,3 @@
-sudo echo "-----start-----" > /home/ubuntu/cicd/deploy.log
-sudo ls . >> /home/ubuntu/cicd/deploy.log
-sudo docker build ../. -t cicd_test
-sudo echo "-----after build" >> /home/ubuntu/cicd/deploy.log
-sudo ls ../ >> /home/ubuntu/cicd/deploy.log
-sudo docker ps -q --filter "name=cicd_test" | grep -q . && docker container kill cicd_test
-sudo docker run -d --rm --name cicd_test -p 8888:8080 cicd_test
+docker build ../. -t cicd_test
+docker ps -q --filter "name=cicd_test" | grep -q . && docker container kill cicd_test
+docker run -d --rm --name cicd_test -p 8888:8080 cicd_test
